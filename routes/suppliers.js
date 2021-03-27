ec6ec9e985e58c5664bd68e72178705d3bab9ceb
Raul Laredo Soria <raul.laredo@jalasoft.com>
2021-03-27 02:49:29 -0400

    Added API

--- routes/suppliers.js
+++ routes/suppliers.js
@@ -24,11 +24,6 @@ router.get('/', function (req, res) {
   })
 //post supplier
 router.post('/', function(req, res, next) {
-  pg.connect( function(err, client) {
-    if (err) {
-      return console.error('error fetching client from pool', err);
-    }
-    console.log("connected to database");
     client.query('INSERT INTO suppliers(supplier_address ,supplier_name ,supplier_contact , supplier_details ) VALUES($1, $2, $3, $4) returning id', [req.body.address, req.body.name, req.body.contact, req.body.details ], function(err, result) {
       if(err) {
         return console.error('error running query', err);
@@ -36,14 +31,8 @@ router.post('/', function(req, res, next) {
       res.send(result);
     });
   });
-});
 //get one supplier
 router.get('/:id', function(req, res, next) {
-  pg.connect( function(err, client) {
-    if (err) {
-      return console.error('error fetching client from pool', err);
-    }
-    console.log("connected to database");
     client.query('SELECT * FROM suppliers WHERE id = $1', [req.params.id], function(err, result) {
       done();
       if (err) {
@@ -52,15 +41,8 @@ router.get('/:id', function(req, res, next) {
       res.send(result);
     });
   });
-});
 // update supplier
 router.put('/:id', function(req, res, next) {
-  pg.connect( function(err, client) {
-    if (err) {
-      return console.error('error fetching client from pool', err);
-    }
-    console.log("connected to database");
-    //compare with .compareSync(req.body.data.attributes.password, storedPW)
     client.query('UPDATE suppliers SET supplier_address = $2, supplier_name = $3, supplier_contact = $4, supplier_details = $5   WHERE id = $1', [req.params.id, req.body.address, req.body.name, req.body.contact, req.body.details ], function(err, result) {
       if (err) {
         return console.error('error running query', err);
@@ -68,14 +50,8 @@ router.put('/:id', function(req, res, next) {
       res.send(result);
     });
   });
-});
 //delete one supplier
 router.delete('/:id', function(req, res, next) {
-  pg.connect(function(err, client) {
-     console.log(conString)
-    if (err) {
-      return console.error('error fetching client from pool', err);
-    }
     console.log("connected to database");
     client.query('DELETE FROM suppliers WHERE id = $1',[req.params.id], function(err, result) {
       if (err) {
@@ -84,6 +60,5 @@ router.delete('/:id', function(req, res, next) {
       res.send(result);
     });
   });
-});
 
 module.exports = router;
