41497fb4aeb52e746a41bb71f9d119e920bd02d0
Raul Laredo Soria <raul.laredo@jalasoft.com>
2021-03-27 03:20:17 -0400

    Added API

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.static(path.join(__dirname, 'public')))
 app .set('views', path.join(__dirname, 'views'))
 app .set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res)=>{
   // res.render('pages/index', {suppliers: actions.getSuppliers()});
